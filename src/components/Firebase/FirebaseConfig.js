837a12db51e8593e687c9dacc136c9dcf206df72
cem-sucu <cems.cems.17@gmail.com>
2022-09-01 11:24:01 +0200

    configuration de firebase V9

---
+++ src/components/Firebase/FirebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const config = {
+    apiKey: "AIzaSyC_g7_zMz59EQbiwJVTXgTf1KgRxyFYvSE",
+    authDomain: "marvel-quiz-ae730.firebaseapp.com",
+    projectId: "marvel-quiz-ae730",
+    storageBucket: "marvel-quiz-ae730.appspot.com",
+    messagingSenderId: "79774977280",
+    appId: "1:79774977280:web:d7c06dced1e350c40437e5",
+};
+
+const app = initializeApp(config);
+
+export default config;
