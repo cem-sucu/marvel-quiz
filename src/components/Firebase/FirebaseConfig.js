a42892ab023239143935c622fa2da60dce2d499c
cem-sucu <cems.cems.17@gmail.com>
2022-09-01 13:37:09 +0200

    authentification via firebase V9

--- src/components/Firebase/FirebaseConfig.js
+++ src/components/Firebase/FirebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const config = {
     apiKey: "AIzaSyC_g7_zMz59EQbiwJVTXgTf1KgRxyFYvSE",
@@ -10,5 +11,6 @@ const config = {
 };
 
 const app = initializeApp(config);
+export const auth = getAuth(app);
 
 export default config;
---
+++ src/components/Firebase/index.js
@@ -0,0 +1,5 @@
+import config from "./firebaseConfig";
+import FirebaseContext from "./context";
+
+export default config;
+export { FirebaseContext };
