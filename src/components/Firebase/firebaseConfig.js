b8f3e21eca41dfb12b9c02b19db471ecdde8fdaf
cem-sucu <cems.cems.17@gmail.com>
2022-09-02 15:47:12 +0200

    authentification version 9 fonctionnelle

---
+++ src/components/Firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Firebase v9
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const config = {
+    apiKey: "AIzaSyC_g7_zMz59EQbiwJVTXgTf1KgRxyFYvSE",
+    authDomain: "marvel-quiz-ae730.firebaseapp.com",
+    projectId: "marvel-quiz-ae730",
+    storageBucket: "marvel-quiz-ae730.appspot.com",
+    messagingSenderId: "79774977280",
+    appId: "1:79774977280:web:d7c06dced1e350c40437e5",
+};
+
+const app = initializeApp(config);
+export const auth = getAuth(app);
+
+export default config;
