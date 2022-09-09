953f048ad88321a33855ff2a7e363f4fea0b56f7
cem-sucu <cems.cems.17@gmail.com>
2022-09-09 15:28:50 +0200

    mise en place du composant Quiz, Levels, Progressbar

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
+import Levels from "../Levels";
+import ProgressBar from "../ProgessBar";
 
-const Quiz = () => {
-    return <div>Quiz</div>;
-};
+class Quiz extends Component {
+    render() {
+        const { pseudo } = this.props.userData;
+
+        return (
+            <div>
+                <Levels />
+                <ProgressBar />
+                <h2>Notre question Quiz</h2>
+                <p className="answerOptions">Question 1</p>
+                <p className="answerOptions">Question 2</p>
+                <p className="answerOptions">Question 3</p>
+                <p className="answerOptions">Question 4</p>
+                <button className="btnSubmit">Suivant</button>
+            </div>
+        );
+    }
+}
 
 export default Quiz;
